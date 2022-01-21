237b165c97733ff1d394107d6760204bbc7aac77
Mark Sayers <marksayers46@icloud.com>
2022-01-21 12:26:29 -0600

    setup dependencies

--- src/Login.js
+++ src/Login.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { useRef, useState, useEffect } from 'react'
 
 const Login = () => {
   
   return (
     <main className="login-box">
         <h1>Sign In</h1>
-        <form>
+        <form className="form-container">
             <label>UserName:</label>
             <input></input>
+            <label>Password:</label>
+            <input></input>
         </form>
+        <button>Sign In</button>
+
     </main>
   );
 };
