8b452c086863a2bd2086f370c4825e021f54d93a
Mark Sayers <marksayers46@icloud.com>
2022-01-21 15:19:49 -0600

    changing up code & adding css styling

--- src/Login.js
+++ src/Login.js
@@ -1,21 +1,58 @@
-import React from 'react';
-import { useRef, useState, useEffect } from 'react'
+import React from "react";
+import { useRef, useState, useEffect } from "react";
 
 const Login = () => {
-  
-  return (
-    <main className="login-box">
-        <h1>Sign In</h1>
-        <form className="form-container">
-            <label>UserName:</label>
-            <input></input>
-            <label>Password:</label>
-            <input></input>
-        </form>
-        <button>Sign In</button>
+  const userRef = useRef();
+  const errRef = useRef();
 
-    </main>
-  );
+  const [user, setUser] = useState("");
+  const [pwd, setPwd] = useState();
+  const [errMsg, setErrMsg] = useState();
+  // success state just for testing purpose
+  const [success, setSuccess] = useState(false);
+
+    useEffect(() => {
+
+    }, []);
+
+    useEffect(() => {
+        setErrMsg("");
+    }, [user, pwd]);
+
+    return (
+        <section>
+            <p ref={errRef} className={errMsg ? "errMsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+            <h1>Sign In</h1>
+            <form>
+                <label htmlFor="username">Username</label>
+                <input
+                    type="text"
+                    id="username"
+                    ref={userRef}
+                    autoComplete="off"
+                    onChange={(e) => setUser(e.target.value)}
+                    value={user}
+                    required
+                />
+                <label htmlFor="password">Password</label>
+                <input
+                    type="text"
+                    id="password"
+                    onChange={(e) => setPwd(e.target.value)}
+                    value={pwd}
+                    required
+                />
+                <button>Sign In</button>
+            </form>
+            <p>
+                Need an Account?<br />
+                <span className="line">
+                    {/*put router link here*/}
+                    <a href="#">Sign Up</a>
+                </span>
+            </p>
+        </section>
+    );
 };
 
 export default Login;
